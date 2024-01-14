ad8c278761b5ecbdde40f6e72a108c5f93ec7247
Pedro Martins <pedromarts.93@gmail.com>
2024-01-14 19:37:31 -0300

    Projeto Finalizado

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 // iniciando projeto
+let totalGeral;
+limpar ();
 
 function adicionar() {
     let produto = document.getElementById ('produto').value;
@@ -10,14 +12,16 @@ function adicionar() {
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
     <section class="carrinho__produtos__produto">
       <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${subTotal}</span>
-    </section>`
-
-    
-    
+    </section>`;
+    totalGeral = totalGeral + subTotal;
+    let campoTotal = document.getElementById ('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;    
 }
 
-
-
 function limpar() {
+    totalGeral = 0;
+    document.getElementById ('lista-produtos').innerHTML = " ";
+    document.getElementById ('valor-total').textContent  = "R$ 0,00";
  
     }

629521802d5d4035504f0abed4a089673d2a9474
Pedro Martins <pedromarts.93@gmail.com>
2024-01-14 18:40:25 -0300

    Adicionando ao carrinho

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 // iniciando projeto
 
 function adicionar() {
-    //recuperar valores nome do produto, quantidade e valor 
-    //calcular o preço, o nosso subtotal 
-    //adicionar no carrinho 
-    //atualizar o valor total
-    let nomeProduto = document.getElementById ('produto');
-    let quantidadeProduto = document.getElementById ('quantidade');
+    let produto = document.getElementById ('produto').value;
+    let nomeProduto = produto.split ('-')[0];
+    let valorUnitario = produto.split ('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let subTotal = valorUnitario * quantidade;
+    let carrinho = document.getElementById ('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
+    <section class="carrinho__produtos__produto">
+      <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${subTotal}</span>
+    </section>`
 
-
+    
+    
 }
+
+
 
 function limpar() {
  

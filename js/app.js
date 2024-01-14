bf0d0f8a041406f77954d818232f1f837df5e884
Pedro Martins <pedromarts.93@gmail.com>
2024-01-14 16:07:42 -0300

    Obtendo o produto adicionado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 // iniciando projeto
+
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor 
+    //calcular o preço, o nosso subtotal 
+    //adicionar no carrinho 
+    //atualizar o valor total
+    let nomeProduto = document.getElementById ('produto');
+    let quantidadeProduto = document.getElementById ('quantidade');
+
+
+}
+
+function limpar() {
+ 
+    }
